0b7b71604759043ac47b24749347ab4f3de26da5
hideo55 <hide.o.j55@gmail.com>
2011-10-27 03:37:52 +0900

    Add profile

---
+++ static/js/favicon.js
@@ -0,0 +1,13 @@
+$(function() {
+  $('.favicon').each(function() {
+    var link = this;
+    var dir = this.href.replace(/^(https?:\/\/[^\/]+\/).*$/, '$1');
+    $('<img/>').attr({
+    width: 16,
+    height: 16,
+    src: dir + '/favicon.ico',
+    border: '0px'
+    }).hide().css('vertical-align', 'baseline').one('load', function() { $(this).show().prependTo(link)
+    });
+  });
+});
