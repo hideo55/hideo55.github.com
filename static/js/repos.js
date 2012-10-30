8269e4c8ad212aa3831681dbdc77cb833683fdb1
hideo55 <hide.o.j55@gmail.com>
2012-10-31 02:13:16 +0900

    Update repos.js for handling JSONP

--- static/js/repos.js
+++ static/js/repos.js
@@ -1,9 +1,9 @@
 $(function() {
   var user = 'hideo55';
   $('#my-repos').html('<img src="static/images/ajax-loader.gif"/>');
-  $.getJSON('https://api.github.com/users/' + user + '/repos?type=owner&sort=updated&callback=?', function(data) {
+  $.getJSON('https://api.github.com/users/' + user + '/repos?type=owner&sort=updated&callback=?', function(res) {
     var buf = [];
-    $.each(data, function(i, repo) {
+    $.each(res.data, function(i, repo) {
       if (!repo.fork)
         buf.push(['<div class="section" id="repo-' + repo.name + '">', '<section>', '<h3 class="subtitle"><a href="', repo.html_url, '">', repo.name, '</a></h3>', '<ul>', '<p class="description">', repo.description, '</p>', '<div class="commits" id="commit-' + repo.name + '"/>', '</ul>', '</section>', '</div>'].join(''));
     });
