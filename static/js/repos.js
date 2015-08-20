23cbe023a33249edbb79c2725e3c21f352692bdc
hideo55 <hide.o.j55@gmail.com>
2015-08-20 12:50:47 +0900

    Change github api

--- static/js/repos.js
+++ static/js/repos.js
@@ -3,8 +3,7 @@ $(function() {
   $('#my-repos').html('<img src="static/images/ajax-loader.gif"/>');
   $.getJSON('https://api.github.com/search/repositories?q=user:' + user, function(res) {
     var buf = [];
-    console.log(res);
-    $.each(res.data.items, function(i, repo) {
+    $.each(res.items, function(i, repo) {
       if (!repo.fork)
         buf.push(['<div class="section" id="repo-' + repo.name + '">', '<section>', '<h3 class="subtitle"><a href="', repo.html_url, '">', repo.name, '</a></h3>', '<ul>', '<p><img src="static/images/star.png" alt="Stars">', repo.stargazers_count, '&nbsp;&nbsp;&nbsp;', '<span class="octicon octicon-repo-forked"></span>', repo.forks_count, '</p>', '<p class="description">', repo.description, '</p>', '</ul>', '</section>', '</div>'].join(''));
     });
