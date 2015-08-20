543225ec2fc144e4d71086bfd552a94c1fa5444d
hideo55 <hide.o.j55@gmail.com>
2015-08-20 12:45:14 +0900

    Change github api

--- static/js/repos.js
+++ static/js/repos.js
@@ -1,12 +1,9 @@
 $(function() {
   var user = 'hideo55';
   $('#my-repos').html('<img src="static/images/ajax-loader.gif"/>');
-  $.getJSON('https://api.github.com/users/' + user + '/repos?type=owner&callback=?', function(res) {
-    res.data.sort(function(a, b) {
-      return b.watchers_count - a.watchers_count;
-    });
+  $.getJSON('https://api.github.com/search/repositories?q=user:' + user, function(res) {
     var buf = [];
-    $.each(res.data, function(i, repo) {
+    $.each(res.data.items, function(i, repo) {
       if (!repo.fork)
         buf.push(['<div class="section" id="repo-' + repo.name + '">', '<section>', '<h3 class="subtitle"><a href="', repo.html_url, '">', repo.name, '</a></h3>', '<ul>', '<p><img src="static/images/star.png" alt="Stars">', repo.stargazers_count, '&nbsp;&nbsp;&nbsp;', '<span class="octicon octicon-repo-forked"></span>', repo.forks_count, '</p>', '<p class="description">', repo.description, '</p>', '</ul>', '</section>', '</div>'].join(''));
     });
